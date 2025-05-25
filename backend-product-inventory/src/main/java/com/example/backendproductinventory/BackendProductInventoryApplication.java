d147507fde1f638465c1434881290c841a64d0d6
Vecii <vecinetflix@gmail.com>
2025-05-25 23:30:16 +0200

    Upload project

---
+++ backend-product-inventory/src/main/java/com/example/backendproductinventory/BackendProductInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendproductinventory;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProductInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendProductInventoryApplication.class, args);
+    }
+}
---
+++ backend-product-inventory/src/test/java/com/example/backendproductinventory/BackendProductInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendproductinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProductInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
